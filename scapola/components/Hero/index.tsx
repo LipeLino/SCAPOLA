f4cd873def1675376fb563b3af051afc7cb1f474
Lipe Lino <104747472+LipeLino@users.noreply.github.com>
2024-09-17 21:47:30 +0000

    Sobe

--- scapola/components/Hero/index.tsx
+++ scapola/components/Hero/index.tsx
@@ -16,19 +16,17 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 Solid - A Complete SaaS Web Template
+                 Assessoria em Relações Públicas e Comunicação
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Free Next.js Template for {"   "}
+                Scapola {"   "} <br />
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  SaaS
+                  Comunica
                 </span>
               </h1>
               <p>
-                Solid Pro - Packed with all the key integrations you need for
-                swift SaaS startup launch, including - Auth, Database, Sanity
-                Blog, Essential Components, Pages and More. Built-winth -
-                Next.js 13, React 18 and TypeScript.
+              Solucionamos demandas de comunicação com um olhar humano e um trabalho artesanal. 
+              Ajudamos a construir a melhor estratégia para sua marca pessoal, profissional ou de sua empresa.               
               </p>
 
               <div className="mt-10">
@@ -38,33 +36,23 @@ const Hero = () => {
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       type="text"
-                      placeholder="Enter your email address"
+                      placeholder="Digite seu e-mail"
                       className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
                     />
                     <button
                       aria-label="get started button"
                       className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
                     >
-                      Get Started
+                      Comece aqui
                     </button>
                   </div>
                 </form>
-
-                <p className="mt-5 text-black dark:text-white">
-                  Try for free no credit card required.
-                </p>
+          
               </div>
             </div>
 
             <div className="animate_right hidden md:w-1/2 lg:block">
               <div className="relative 2xl:-mr-7.5">
-                <Image
-                  src="/images/shape/shape-01.png"
-                  alt="shape"
-                  width={46}
-                  height={246}
-                  className="absolute -left-11.5 top-0"
-                />
                 <Image
                   src="/images/shape/shape-02.svg"
                   alt="shape"
@@ -88,7 +76,7 @@ const Hero = () => {
                   />
                   <Image
                     className="hidden shadow-solid-l dark:block"
-                    src="/images/hero/hero-dark.svg"
+                    src="/images/hero/hero-dark.png"
                     alt="Hero"
                     fill
                   />
